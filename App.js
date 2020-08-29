3e21e175bb599def6a11768902657acd39dc3ef6
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-29 11:32:27 +0530

    intial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+/**
+ * This is a simple to do app
+ * example of redux
+ */
+import React from 'react';
+import Home from './src/screens/Home';
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Home() {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text style={{color: '#000000', fontSize: 20}}>Home</Text>
+    </View>
+  );
+}
+
+export default Home;
