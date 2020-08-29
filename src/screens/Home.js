a5302f270adec0cbfaa89de4e6cd7301918f6600
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-29 13:41:43 +0530

    counter with states

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 function Home() {
+  const [counts, setCounts] = useState(0);
+
+  const increment = () => {
+    setCounts(counts + 1);
+  };
+
   return (
     <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Text style={{color: '#000000', fontSize: 20}}>Home</Text>
+      <Text style={styles.TextStyle}>{counts}</Text>
+      <Text style={styles.TextStyle}>Counts</Text>
+      <Button title="Add" onPress={increment}></Button>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  TextStyle: {
+    color: '#000000',
+    fontSize: 20,
+  },
+});
 export default Home;
